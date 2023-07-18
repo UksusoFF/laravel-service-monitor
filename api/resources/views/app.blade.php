6ffc6552b1e441dd8e12689a0b27d961e8e1c46f
Uksusov Kirill <k.uksusov@ya.ru>
2023-07-18 14:41:13 +0400

    Favicon updated

--- api/resources/views/app.blade.php
+++ api/resources/views/app.blade.php
@@ -2,17 +2,16 @@
 <html lang="en-US">
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="host" content="{{ config('app.url') }}">
-
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="icon" href="/favicon.ico" />
-
-    <script type="module" crossorigin src="/assets/index.js?{{ config('version.hash') }}"></script>
-    <link rel="stylesheet" href="/assets/index.css?{{ config('version.hash') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="icon" href="/favicon/favicon.ico"/>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
-<div id="app"></div>
+<div style="height: 100vh;display: flex;flex-direction: column;">
+    <img src="/favicon/favicon.png" style="max-width:150px;margin: auto;">
+    <p>{{ config('app.name') }}</p>
+</div>
 </body>
 </html>
