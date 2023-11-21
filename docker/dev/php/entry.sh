d1bf4220e8c1a8812b1699b26c2c8ae110e36d83
Uksusov Kirill <k.uksusov@ya.ru>
2023-11-21 10:36:10 +0400

    Migrate database after container up

--- docker/dev/php/entry.sh
+++ docker/dev/php/entry.sh
@@ -19,6 +19,14 @@ until db_ready; do
 done
 >&2 echo "Database is available"
 
+php artisan cache:clear
+
+php artisan migrate --force
+
+php artisan route:clear
+php artisan view:clear
+php artisan config:clear
+
 nohup php artisan schedule:work &>> /var/www/api/storage/logs/artisan.log &
 
 php-fpm
