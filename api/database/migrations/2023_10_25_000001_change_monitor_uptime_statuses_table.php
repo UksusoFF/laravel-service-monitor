9db3ee05fcfc0f8b54363169337bc33e678e984d
Kirill Uksusov <uksusoff@yandex.ru>
2024-12-23 13:57:00 +0400

    Code style

--- api/database/migrations/2023_10_25_000001_change_monitor_uptime_statuses_table.php
+++ api/database/migrations/2023_10_25_000001_change_monitor_uptime_statuses_table.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Schema\Blueprint;
 return new class() extends Migration {
     public function up(): void
     {
-        Schema::table('monitor_uptime_statuses', function (Blueprint $table) {
+        Schema::table('monitor_uptime_statuses', function(Blueprint $table) {
             $table->longText('uptime_check_failure_reason')->change();
         });
 
-        Schema::table('monitor_certificate_statuses', function (Blueprint $table) {
+        Schema::table('monitor_certificate_statuses', function(Blueprint $table) {
             $table->longText('certificate_check_failure_reason')->change();
         });
     }
